732d68e899f3755a1e18912455ba5aef87ace594
Nihat Babazade <nihad@agdevelopments.net>
2020-03-18 17:20:38 +0400

    nope2

--- src/NewAd.js
+++ src/NewAd.js
@@ -55,7 +55,7 @@ export default class Sign extends Component {
       
 
       componentDidMount() {
-        fetch('https://dooaz.herokuapp.com/api/elanlar/newad',{
+        fetch('https://192.168.88.253:8080/api/elanlar/newad',{
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded',
                 'Accept': 'application/json'
